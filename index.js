2efc97fc8e372b4350a5571c5e17a74bec0e4219
Shanmukh <lifesaver0510@gmail.com>
2026-01-02 12:01:32 +0530

    check

--- index.js
+++ index.js
@@ -140,5 +140,5 @@ app.delete('/products/:id', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT} `);
 });
